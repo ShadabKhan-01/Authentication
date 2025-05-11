187bcb9f3ff9ddabb3264475bc01ea5fd6d876af
Shadab Khan <iam.shadab.khan.1001@gmail.com>
2025-05-11 15:51:16 +0530

    login backend setup

---
+++ src/app/api/user/login/route.ts
@@ -0,0 +1,62 @@
+import { connectDB } from "@/dbConfig/dbConfig";
+import User from "@/model/userModel";
+import bcrypt from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+connectDB();
+
+export async function POST(request: NextRequest) {
+    try {
+        // get the request body
+        const reqBody = await request.json();
+        const { email, password } = reqBody;
+
+        // Validate request body
+        if (!email || !password) {
+            console.log("Invalid request body:", reqBody);
+            return NextResponse.json({ message: "Please provide all fields" }, { status: 400 });
+        }
+
+        // Find user by email
+        const user = await User.findOne({ email });
+        if (!user) {
+            console.log(`User not found with email: ${email}`);
+            return NextResponse.json({ message: "Invalid email or password" }, { status: 400 });
+        }
+
+        // Check if password matches
+        const isValidPassword = await bcrypt.compare(password, user.password)
+        if (!isValidPassword) {
+            console.log("Invalid password for user:", email);
+            return NextResponse.json({ message: "Invalid email or password" }, { status: 400 });
+        }
+
+        // Generate JWT token
+        const tokenData = {
+            id: user._id,
+            email: user.email,
+            name: user.name,
+        }
+
+        const token = await jwt.sign(tokenData, process.env.JWT_SECRET as string, {expiresIn: "1d"});
+
+        const response = NextResponse.json({
+            message: "Login successful",
+            success: true,
+            user: {
+                name: user.name,
+                email: user.email,
+            },
+        });
+        // response.cookies.set("token", token,{httpOnly: true, secure: true, sameSite: "strict", path: "/"});
+        response.cookies.set("token", token,{httpOnly: true});
+
+        return response;
+        
+        
+    } catch (error) {
+        console.error("Error in user login POST request:", error);
+        return NextResponse.json({ error: "Something went wrong while logging in" }, { status: 500 });
+    }
+}
